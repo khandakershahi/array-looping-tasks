2b89c0fc0b934d5cd3a304aef8284f4edeca05f2
khandakershahi <khandakershahi@gmail.com>
2025-08-07 20:20:38 +0600

    task 4 done

--- task-4.js
+++ task-4.js
@@ -14,3 +14,18 @@ Reverse the words of a sentence. Only the position of the word will be reversed.
  */
 
 const statement = 'I am a hard working person';
+
+const new_statement = statement.split(" ");
+
+console.log(new_statement);
+
+const rev_statement = [];
+
+for(let i = new_statement.length-1; i >= 0; i--){
+    rev_statement.push(new_statement[i]);
+}
+
+console.log(rev_statement);
+const rev_statement_string = rev_statement.join(' ');
+console.log(rev_statement_string);
+
