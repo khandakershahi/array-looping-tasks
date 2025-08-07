3185c9fc497afe13cadbf4fba96197a2b40fb761
khandakershahi <khandakershahi@gmail.com>
2025-08-07 19:26:00 +0600

    task-3 done

---
+++ task-3.js
@@ -0,0 +1,28 @@
+/**
+ * ### Task 3
+
+Use a for...of loop to concatenate all the elements of an array into a single string.
+
+**Input:**
+`var numbers = ['Tom', 'Tim', 'Tin', 'Tik']`
+
+**Output:**
+
+`'TomTimTinTik'`
+
+---
+
+ */
+
+var numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
+
+ var joinString = '';
+
+for(var num of numbers){
+    
+    joinString += num;
+}
+
+console.log(joinString);
+// console.log(joinString);
+
